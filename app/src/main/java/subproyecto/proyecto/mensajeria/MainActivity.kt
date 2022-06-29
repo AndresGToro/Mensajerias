e48ac1cd3f79428ebae4bcf5cc3da3f431033b14
AndresGToro <andres6azul@gmail.com>
2022-06-29 16:12:35 -0500

    Version 1.23

--- app/src/main/java/subproyecto/proyecto/mensajeria/MainActivity.kt
+++ app/src/main/java/subproyecto/proyecto/mensajeria/MainActivity.kt
@@ -16,7 +16,7 @@ import subproyecto.proyecto.mensajeria.fragments.RatesFragment
 class MainActivity : ToolbarActivity() {
 
     //probando
-    
+
     private var prevBottomSelected: MenuItem? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
